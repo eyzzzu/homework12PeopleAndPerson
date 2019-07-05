3ec83d75c146253d8dbc18304cb42bc7c7dd55c1
eyzzzu <eyzzzu@gmail.com>
2019-07-05 19:19:51 +0300

    Innitial commit

--- src/com/homework/Main.java
+++ src/com/homework/Main.java
@@ -16,7 +16,6 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-
         PeopleList.readFile();          // считываем имена и фамилии из подготовленного тхт файла
         // и создаем два списка(список имен и фамилий)
 
